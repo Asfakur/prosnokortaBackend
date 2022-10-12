dd9d994427e107d8bd737556996451b3b47401d6
Asfakur <saikat.hap@gmail.com>
2022-10-12 19:55:20 +0600

    cloudcuster db

--- prosnokorta2/settings/dev.py
+++ prosnokorta2/settings/dev.py
@@ -14,10 +14,10 @@ DATABASES = {
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': '',
-        # 'NAME': 'freedb_prosnokorta4',
-        # 'HOST': 'sql.freedb.tech',
-        # 'USER': 'freedb_abulkuddus343',
-        # 'PASSWORD': 'S2e6@aK$Rt$x3Du',
+        # 'NAME': 'prosnokorta2',
+        # 'HOST': 'mysql-91149-0.cloudclusters.net',
+        # 'USER': 'sakdfj45934jfjs',
+        # 'PASSWORD': 'skdjf*&*$Wskdjf',
         # 'OPTIONS': {
         #     'init_command': 'SET default_storage_engine=INNODB',
         #     # 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
--- prosnokorta2/settings/prod.py
+++ prosnokorta2/settings/prod.py
@@ -11,10 +11,17 @@ ALLOWED_HOSTS = ['prosnokorta.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'freedb_prosnokorta4',
-        'HOST': 'sql.freedb.tech',
-        'USER': 'freedb_abulkuddus343',
-        'PASSWORD': 'S2e6@aK$Rt$x3Du',
+        
+        # 'NAME': 'freedb_prosnokorta4',
+        # 'HOST': 'sql.freedb.tech',
+        # 'USER': 'freedb_abulkuddus343',
+        # 'PASSWORD': 'S2e6@aK$Rt$x3Du',
+
+        'NAME': 'prosnokorta2',
+        'HOST': 'mysql-91149-0.cloudclusters.net',
+        'USER': 'sakdfj45934jfjs',
+        'PASSWORD': 'skdjf*&*$Wskdjf',
+
         # 'OPTIONS': {
         #     'init_command': 'SET default_storage_engine=INNODB',
         #     # 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
